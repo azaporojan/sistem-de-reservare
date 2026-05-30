ed11d80130988891b90decc481c64af957889468
azaporojan <alexanderzaporo@gmail.com>
2026-05-30 13:26:03 +0300

    Initial commit

---
+++ src/main/kotlin/md/usm/teza/reservare/SistemDeReservareApplication.kt
@@ -0,0 +1,11 @@
+package md.usm.teza.reservare
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SistemDeReservareApplication
+
+fun main(args: Array<String>) {
+    runApplication<SistemDeReservareApplication>(*args)
+}
---
+++ src/test/kotlin/md/usm/teza/reservare/SistemDeReservareApplicationTests.kt
@@ -0,0 +1,13 @@
+package md.usm.teza.reservare
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SistemDeReservareApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
